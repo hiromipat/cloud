b04f1793cffe4b8d5bf220fd019742e42a3129e5
peromero <hiromipat123123@gmail.com>
2021-01-07 16:32:55 +0100

    region add

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 terraform {
   required_version = ">= 0.12"
 }
+provider "aws" {
+  region  = "eu-west-1"
+}
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-peromero"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+  
 }
